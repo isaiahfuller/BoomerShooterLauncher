5bbed1275daad1f696fef63058a02ed1973f84b1
Isaiah Fuller <fuller.spectrum@gmail.com>
2022-05-09 12:20:38 -0400

    updated modpack ui

--- mods_view.py
+++ mods_view.py
@@ -15,7 +15,8 @@ class ModsView(QtWidgets.QMainWindow):
                 self.settings = QtCore.QSettings(
                     "fullerSpectrum", "Boomer Shooter Launcher")
             case "Linux":
-                self.settings = QtCore.QSettings("boomershooterlauncher", "config")
+                self.settings = QtCore.QSettings(
+                    "boomershooterlauncher", "config")
 
         self.mods = {
             "name": "Mod name",
@@ -25,92 +26,96 @@ class ModsView(QtWidgets.QMainWindow):
 
         self.setWindowTitle("Modpack Builder")
 
-        self.game_list = self.parent()
-        self.games = self.game_list.games
+        self.gameList = self.parent()
+        self.games = self.gameList.games
 
-        main_layout = QtWidgets.QVBoxLayout()
-        header_line = QtWidgets.QHBoxLayout()
-        centerHBox = QtWidgets.QHBoxLayout()
-        self.center_left_list = QtWidgets.QListWidget()
-        center_right_grid = QtWidgets.QGridLayout()
-        centerVBox = QtWidgets.QVBoxLayout()
-        self.up_button = QtWidgets.QPushButton("▲", self)
-        self.down_button = QtWidgets.QPushButton("▼", self)
-        footer_buttons = QtWidgets.QHBoxLayout()
+        mainLayout = QtWidgets.QVBoxLayout()
+        header = QtWidgets.QHBoxLayout()
+        modInfoBox = QtWidgets.QHBoxLayout()
+        self.modList = QtWidgets.QListWidget()
+        modInfoGrid = QtWidgets.QGridLayout()
+        self.upButton = QtWidgets.QPushButton("▲", self)
+        self.downButton = QtWidgets.QPushButton("▼", self)
+        footer = QtWidgets.QHBoxLayout()
 
-        main_layout.addLayout(header_line)
+        moveButtons = QtWidgets.QVBoxLayout()
+        moveButtons.setAlignment(QtCore.Qt.AlignTop)
+        self.upButton.setMaximumWidth(35)
+        self.downButton.setMaximumWidth(35)
+        self.upButton.setDisabled(True)
+        self.downButton.setDisabled(True)
 
-        main_layout.addLayout(centerHBox)
+        self.baseSelect = QtWidgets.QComboBox()
+        self.nameEdit = QtWidgets.QLineEdit()
 
-        centerHBox.addWidget(self.center_left_list)
-        centerHBox.addLayout(centerVBox)
-        centerHBox.addLayout(center_right_grid)
-        centerHBox.setContentsMargins(0, 0, 0, 0)
-        centerHBox.setAlignment(QtCore.Qt.AlignCenter)
+        baseLabel = QtWidgets.QLabel("Base game: ")
+        nameLabel = QtWidgets.QLabel("Name: ")
 
-        centerVBox.setAlignment(QtCore.Qt.AlignVCenter)
-        centerVBox.addWidget(self.up_button)
-        centerVBox.addWidget(self.down_button)
+        mainLayout.addLayout(header)
 
-        self.base_combobox = QtWidgets.QComboBox()
-        self.name_edit = QtWidgets.QLineEdit()
-        base_label = QtWidgets.QLabel("Base game: ")
-        name_label = QtWidgets.QLabel("Name: ")
+        mainLayout.addWidget(self.modList)
+        mainLayout.addLayout(modInfoBox)
 
-        header_line.addWidget(name_label)
-        header_line.addWidget(self.name_edit)
-        header_line.addWidget(base_label)
-        header_line.addWidget(self.base_combobox)
-        header_line.setAlignment(QtCore.Qt.AlignJustify)
-        self.base_combobox.setStyleSheet("QComboBox{min-width: 200px;}")
-        self.name_edit.setStyleSheet("QLineEdit{min-width: 150px;}")
+        moveButtons.addWidget(self.upButton)
+        moveButtons.addWidget(self.downButton)
+
+        modInfoBox.addLayout(moveButtons)
+        modInfoBox.addLayout(modInfoGrid)
+
+        header.addWidget(nameLabel)
+        header.addWidget(self.nameEdit)
+        header.addWidget(baseLabel)
+        header.addWidget(self.baseSelect)
+        header.setAlignment(QtCore.Qt.AlignJustify)
+        self.nameEdit.setStyleSheet("QLineEdit{min-width: 130px;}")
+        self.baseSelect.setSizeAdjustPolicy(
+            QtWidgets.QComboBox.AdjustToContents)
 
         scroll = QtWidgets.QScrollArea()
-        scroll.setLayout(main_layout)
+        scroll.setLayout(mainLayout)
 
-        self.mod_path_label = QtWidgets.QLabel("")
-        self.mod_path_label.setWordWrap(True)
+        self.pathLabel = QtWidgets.QLabel("")
+        self.pathLabel.setWordWrap(True)
 
-        center_right_grid.setAlignment(QtCore.Qt.AlignTop)
-        center_right_grid.addWidget(QtWidgets.QLabel("Name: "), 0, 0)
-        center_right_grid.addWidget(QtWidgets.QLabel("Source: "), 1, 0)
-        center_right_grid.addWidget(QtWidgets.QLabel("Path: "), 2, 0)
+        modInfoGrid.setAlignment(QtCore.Qt.AlignTop)
+        modInfoGrid.addWidget(QtWidgets.QLabel("Name: "), 0, 0)
+        modInfoGrid.addWidget(QtWidgets.QLabel("Source: "), 1, 0)
+        modInfoGrid.addWidget(QtWidgets.QLabel("Path: "), 2, 0)
 
-        self.mod_name_edit = QtWidgets.QLineEdit()
-        self.mod_source_edit = QtWidgets.QLineEdit()
-        self.mod_source_edit.setStyleSheet("min-width: 150px;")
-        center_right_grid.addWidget(self.mod_name_edit, 0, 1)
-        center_right_grid.addWidget(self.mod_source_edit, 1, 1)
-        center_right_grid.addWidget(self.mod_path_label, 2, 1)
+        self.modNameEdit = QtWidgets.QLineEdit()
+        self.modSourceEdit = QtWidgets.QLineEdit()
+        self.modSourceEdit.setStyleSheet("min-width: 150px;")
+        modInfoGrid.addWidget(self.modNameEdit, 0, 1)
+        modInfoGrid.addWidget(self.modSourceEdit, 1, 1)
+        modInfoGrid.addWidget(self.pathLabel, 2, 1)
 
-        add_file_button = QtWidgets.QPushButton("Add", self)
-        remove_file_button = QtWidgets.QPushButton("Remove", self)
-        save_button = QtWidgets.QPushButton("Save", self)
-        footer_buttons.addWidget(add_file_button)
-        footer_buttons.addWidget(remove_file_button)
-        footer_buttons.addWidget(save_button)
-        main_layout.addLayout(footer_buttons)
+        addFileButton = QtWidgets.QPushButton("Add", self)
+        removeFileButton = QtWidgets.QPushButton("Remove", self)
+        saveButton = QtWidgets.QPushButton("Save", self)
+        footer.addWidget(addFileButton)
+        footer.addWidget(removeFileButton)
+        footer.addWidget(saveButton)
+        mainLayout.addLayout(footer)
 
-        self.file_chooser = QtWidgets.QFileDialog(self)
-        self.file_chooser.setFileMode(QtWidgets.QFileDialog.ExistingFiles)
+        self.fileChooser = QtWidgets.QFileDialog(self)
+        self.fileChooser.setFileMode(QtWidgets.QFileDialog.ExistingFiles)
 
         self.baseComboBuilder()
 
         self.setAcceptDrops(True)
 
-        self.base_combobox.currentTextChanged.connect(self.baseChanged)
-        self.center_left_list.currentRowChanged.connect(
-            self.selectedModChanged)
+        self.baseSelect.currentTextChanged.connect(self.baseChanged)
+        self.modList.currentRowChanged.connect(self.selectedModChanged)
 
-        add_file_button.clicked.connect(self.addMod)
-        remove_file_button.clicked.connect(self.removeMod)
+        addFileButton.clicked.connect(self.addMod)
+        removeFileButton.clicked.connect(self.removeMod)
 
-        self.up_button.clicked.connect(self.moveUp)
-        self.down_button.clicked.connect(self.moveDown)
-        save_button.clicked.connect(self.saveFile)
-        self.name_edit.textEdited.connect(self.changeName)
-        self.mod_name_edit.textEdited.connect(self.changeModName)
-        self.mod_source_edit.textEdited.connect(self.changeModSource)
+        self.upButton.clicked.connect(self.moveUp)
+        self.downButton.clicked.connect(self.moveDown)
+        saveButton.clicked.connect(self.saveFile)
+        self.nameEdit.textEdited.connect(self.changeName)
+        self.modNameEdit.textEdited.connect(self.changeModName)
+        self.modSourceEdit.textEdited.connect(self.changeModSource)
 
         self.setCentralWidget(scroll)
 
@@ -140,18 +145,18 @@ class ModsView(QtWidgets.QMainWindow):
         if not found:
             self.mods["files"].append(
                 {"name": modName, "path": filePath, "source": ""})
-            self.mod_name_edit.setText(modName)
-            self.mod_path_label.setText(filePath)
-            self.center_left_list.addItem(fileName)
+            self.modNameEdit.setText(modName)
+            self.pathLabel.setText(filePath)
+            self.modList.addItem(fileName)
 
     def baseComboBuilder(self):
-        self.base_combobox.clear()
+        self.baseSelect.clear()
         self.settings.beginGroup("Games")
         bases = []
         for game in self.settings.childGroups():
             if self.settings.value(f"{game}/game") not in bases:
                 bases.append(self.settings.value(f"{game}/game"))
-                self.base_combobox.addItem(self.settings.value(f"{game}/game"))
+                self.baseSelect.addItem(self.settings.value(f"{game}/game"))
         self.settings.endGroup()
         bases.sort()
         self.mods["base"] = bases[0]
@@ -160,34 +165,38 @@ class ModsView(QtWidgets.QMainWindow):
         self.mods["base"] = text
 
     def selectedModChanged(self, currentRow):
-        self.mod_name_edit.setText(self.mods["files"][currentRow]["name"])
-        self.mod_source_edit.setText(self.mods["files"][currentRow]["source"])
-        self.mod_path_label.setText(self.mods["files"][currentRow]["path"])
+        self.modNameEdit.setText(self.mods["files"][currentRow]["name"])
+        self.modSourceEdit.setText(self.mods["files"][currentRow]["source"])
+        self.pathLabel.setText(self.mods["files"][currentRow]["path"])
         self.selected = currentRow
-        row = self.center_left_list.currentRow()
-        if row == 0: self.up_button.setDisabled(True)
-        else: self.up_button.setDisabled(False)
-        if row == self.center_left_list.count() - 1: self.down_button.setDisabled(True)
-        else: self.down_button.setDisabled(False)
+        row = self.modList.currentRow()
+        if row == 0:
+            self.upButton.setDisabled(True)
+        else:
+            self.upButton.setDisabled(False)
+        if row == self.modList.count() - 1:
+            self.downButton.setDisabled(True)
+        else:
+            self.downButton.setDisabled(False)
 
     def removeMod(self):
-        row = self.center_left_list.currentRow()
-        self.center_left_list.takeItem(row)
+        row = self.modList.currentRow()
+        self.modList.takeItem(row)
         self.mods["files"].pop(row)
 
     def addMod(self):
-        files = self.file_chooser.getOpenFileUrls()
+        files = self.fileChooser.getOpenFileUrls()
         for file in files[0]:
             self.addFileToList(str(file.toLocalFile()))
 
     def changeModPosition(self, i):
-        row = self.center_left_list.currentRow()
-        tempRow = self.center_left_list.takeItem(row+i)
+        row = self.modList.currentRow()
+        tempRow = self.modList.takeItem(row+i)
         tempObj = self.mods["files"][row+i]
-        self.center_left_list.insertItem(row, tempRow)
+        self.modList.insertItem(row, tempRow)
         self.mods["files"][row+i] = self.mods["files"][row]
         self.mods["files"][row] = tempObj
-        self.selected = self.center_left_list.currentRow()
+        self.selected = self.modList.currentRow()
 
     def moveUp(self):
         self.changeModPosition(-1)
@@ -229,14 +238,14 @@ class ModsView(QtWidgets.QMainWindow):
         self.show()
 
     def closeEvent(self, event: QtGui.QCloseEvent) -> None:
-        self.game_list.refresh()
+        self.gameList.refresh()
         return super().closeEvent(event)
 
     def openFile(self):
-        name = self.game_list.selectedItems()[1].text()
+        name = self.gameList.selectedItems()[1].text()
         self.settings.beginGroup(f"Modpacks/{name}")
-        self.name_edit.setText(name)
-        self.base_combobox.setCurrentText(self.settings.value("base"))
+        self.nameEdit.setText(name)
+        self.baseSelect.setCurrentText(self.settings.value("base"))
         self.mods["name"] = name
         self.mods["base"] = self.settings.value("base")
         size = self.settings.beginReadArray("files")
@@ -244,18 +253,18 @@ class ModsView(QtWidgets.QMainWindow):
             self.settings.setArrayIndex(i)
             fileSplit = self.settings.value("path").split(os.sep)
             fileName = fileSplit[len(fileSplit) - 1]
-            self.center_left_list.addItem(fileName)
+            self.modList.addItem(fileName)
             self.mods["files"].append({"name": self.settings.value(
                 "name"), "path": self.settings.value("path"), "source": self.settings.value("source")})
         self.settings.endArray()
-        self.mod_name_edit.setText(self.settings.value("files/1/name"))
-        self.mod_path_label.setText(self.settings.value("files/1/path"))
-        self.mod_source_edit.setText(self.settings.value("files/1/source"))
+        self.modNameEdit.setText(self.settings.value("files/1/name"))
+        self.pathLabel.setText(self.settings.value("files/1/path"))
+        self.modSourceEdit.setText(self.settings.value("files/1/source"))
         self.settings.endGroup()
         self.showWindow()
 
     def rmFile(self):
-        name = self.game_list.selectedItems()[1].text()
+        name = self.gameList.selectedItems()[1].text()
         self.settings.remove(f"Modpacks/{name}")
         self.logger.info(f"Removing {name}")
-        self.game_list.refresh()
+        self.gameList.refresh()
